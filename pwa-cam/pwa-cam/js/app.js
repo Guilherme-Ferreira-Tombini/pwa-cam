911b859b10aaf24002dbded27c528b167ce4893c
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-10-23 13:23:29 -0300

    commit inicial

---
+++ pwa-cam/pwa-cam/js/app.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+    "use strict";   
+    if("serviceWorker" in navigator){
+        navigator.serviceWorker.register("./sw.js");
+    }
+};
+
+
+// configurando as constraintes do video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+// capturando os elementos em tela
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+
+//Estabelecendo o acesso a camera e inicializando a visualização
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+// https://pastebin.com/nuzyWXHS   ->css
+// https://pastebin.com/bNuD4EwD   ->js
+
+
+// Função para tirar foto
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+// carrega imagem de camera quando a janela carregar
+window.addEventListener("load", cameraStart, false);
